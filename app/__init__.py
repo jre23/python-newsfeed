eb82ccf520c0ee60316b56ac81e85a21241291b4
jre23 <jre.estrada@gmail.com>
2021-03-31 20:00:04 -0700

    set up app package and app config

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app(test_config=None):
+  # app config
+  app = Flask(__name__, static_url_path='/') # serve any static resources from the root directory
+  app.url_map.strict_slashes = False # trailing slashes are optional
+  app.config.from_mapping(
+    SECRET_KEY="super_secret_key" # used when creating server-side sessions
+  )
+
+  return app
