acb6f3a9d2a7f315e08a3a2af1caab8c52d57794
jre23 <jre.estrada@gmail.com>
2021-04-02 19:35:55 -0700

    added a User model and inserted seed data into mysql database

---
+++ seeds.py
@@ -0,0 +1,23 @@
+from app.models import User
+from app.db import Session, Base, engine
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+# from sqlalchemy.orm import sessionmaker
+# Session = sessionmaker(bind=engine)
+# Session variable generates temporary connections for performing CRUD operations
+db = Session()
+
+# insert users
+db.add_all([
+  User(username='alesmonde0', email='nwestnedge0@cbc.ca', password='password123'),
+  User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
+  User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
+  User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
+  User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+])
+
+db.commit()
+
+db.close()
