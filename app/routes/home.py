14ffb877ec823ba39e2561fc04061c38ca12f93c
jre23 <jre.estrada@gmail.com>
2021-04-20 17:17:59 -0700

    added usage of session boolean to root and post routes, redirect login route to dashboard if user already logged in

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -14,11 +14,18 @@ def index():
     db.query(Post).order_by(Post.created_at.desc()).all()
   )
   # render all posts to homepage template
-  return render_template('homepage.html', posts=posts)
+  return render_template(
+    'homepage.html',
+    posts=posts,
+    loggedIn=session.get('loggedIn'))
 
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+  # if user not already logged in, render login page, else redirect to dashboard
+  if session.get('loggedIn') is None:
+    return render_template('login.html')
+
+  return redirect('/dashboard')
 
 # <id> parameter
 @bp.route('/post/<id>')
@@ -27,4 +34,7 @@ def single(id):
   db = get_db()
   post = db.query(Post).filter(Post.id == id).one()
   # render single post to single-post template
-  return render_template('single-post.html', post=post)
+  return render_template(
+    'single-post.html',
+    post=post,
+    loggedIn=session.get('loggedIn'))
