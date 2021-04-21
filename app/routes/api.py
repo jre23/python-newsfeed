48a99fa5b31d4429128a933c07256dc861ef0370
jre23 <jre.estrada@gmail.com>
2021-04-20 19:29:53 -0700

    added comment and upvote api routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request, jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
+# matches with /api/users
 @bp.route('/users', methods=['POST'])
 def signup():
   data = request.get_json()
@@ -36,27 +37,80 @@ def signup():
 
   return jsonify(id = newUser.id)
 
+# matches with /api/users/logout
 @bp.route('/users/logout', methods=['POST'])
 def logout():
   # remove session variables
   session.clear()
   return '', 204
 
+# matches with /api/users/login
 @bp.route('/users/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    db = get_db()
+  data = request.get_json()
+  db = get_db()
     
-    try:
-      user = db.query(User).filter(User.email == data['email']).one()
-    except:
-      print(sys.exc_info()[0])
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+  except:
+    print(sys.exc_info()[0])
 
-      if user.verify_password(data['password']) == False:
-        return jsonify(message = 'Incorrect credentials'), 400
+    if user.verify_password(data['password']) == False:
+      return jsonify(message = 'Incorrect credentials'), 400
         
-    session.clear()
-    session['user_id'] = user.id
-    session['loggedIn'] = True
+  session.clear()
+  session['user_id'] = user.id
+  session['loggedIn'] = True
 
-    return jsonify(id = user.id)
+  return jsonify(id = user.id)
+
+# matches with /api/comments
+@bp.route('/comments', methods=['POST'])
+def comment():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new comment. comment_text and post_id come from the front-end. user_id is from the session
+    newComment = Comment(
+      comment_text = data['comment_text'],
+      post_id = data['post_id'],
+      user_id = session.get('user_id')      
+    )
+    db.add(newComment)
+    db.commit()
+  except:
+    print(sys.exc_info()[0], 'Comment failure')
+
+    db.rollback()
+    return jsonify(message = 'Comment failed'), 500
+
+  return jsonify(id = newComment.id)
+
+# matches with /api/posts/upvotes
+@bp.route('/posts/upvote', methods=['PUT'])
+def upvote():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    newVote = Vote(
+      post_id = data['post_id'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newVote)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Upvote failed'), 500
+  
+  return '', 204
+
+  
+# @bp.route('/posts', methods=['POST'])
+# def post():
+#   data = request.get_json()
+#   db = get_db()
